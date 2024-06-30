8a49bd37e72e7c1bc9aaa313dce0b97e146416ca
bqmb3 <a0hubh4t@duck.com>
2024-06-30 14:13:33 +0900

    test 2

--- main.lua
+++ main.lua
@@ -39,7 +39,7 @@ function F3X.new(init)
             assert(ServerEndpoint, errors.endpointNotFound)
             self._core = Core
             self._endpoint = ServerEndpoint
-            self:SetParent({Folder}, plr)
+            self:SetParent({Folder}, workspace)
         end,
         _core = nil,
         _endpoint = nil,
